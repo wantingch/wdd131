326df6902437ac4f16d0b152ee15c03e3ba9cd07
tinachen <wanting@byui.edu>
2024-05-02 09:24:32 -0600

    statment2

---
+++ mission.js
@@ -0,0 +1,22 @@
+let themeSelector = document.getElementById("mode")
+function changeTheme() {
+    let theme=themeSelector.value
+if (theme=="dark"){
+    let body=document.getElementById("body")
+    console.log(body)
+    body.classList.add("dark")
+    let logo=document.getElementById("logo")
+    logo.setAttribute("src", "./byui-logo_white.png")
+}else{
+    let body=document.getElementById("body")
+    console.log(body)
+    body.classList.remove("dark")
+    let logo=document.getElementById("logo")
+    logo.setAttribute("src", "./byui-logo_blue.webp")
+}
+}
+console.log(themeSelector)
+themeSelector = document.getElementById("mode")
+
+themeSelector.addEventListener("cshange", changeTheme)
+// add eventlistener to the themeSelector element here. Use the changeTheme function as the event handler function.
