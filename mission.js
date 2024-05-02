445402b587d299b4f7b2e8f2cbfbebc6188ad3e8
tinachen <wanting@byui.edu>
2024-05-02 09:29:04 -0600

    123

--- mission.js
+++ mission.js
@@ -3,19 +3,19 @@ function changeTheme() {
     let theme=themeSelector.value
 if (theme=="dark"){
     let body=document.getElementById("body")
-    console.log(body)
+    
     body.classList.add("dark")
     let logo=document.getElementById("logo")
     logo.setAttribute("src", "./byui-logo_white.png")
 }else{
     let body=document.getElementById("body")
-    console.log(body)
+    
     body.classList.remove("dark")
     let logo=document.getElementById("logo")
     logo.setAttribute("src", "./byui-logo_blue.webp")
 }
 }
-console.log(themeSelector)
+
 themeSelector = document.getElementById("mode")
 
 themeSelector.addEventListener("cshange", changeTheme)
